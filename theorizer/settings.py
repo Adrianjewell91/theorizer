71a7c1e6750d82893e603825c09f7a393218f0ad
Adrian <adrianjewell91@gmail.com>
2017-11-26 15:35:57 -0800

    add frontend start

--- theorizer/settings.py
+++ theorizer/settings.py
@@ -77,10 +77,21 @@ WSGI_APPLICATION = 'theorizer.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'theorizer',
+        'USER': 'theorizer_admin',
+        'PASSWORD': 'password',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
