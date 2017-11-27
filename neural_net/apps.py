558770f964aedc3b06534b398ca1f124aecb9755
Adrian <adrianjewell91@gmail.com>
2017-11-26 17:30:55 -0800

    start adding nn

---
+++ neural_net/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class NeuralNetConfig(AppConfig):
+    name = 'neural_net'
--- theorizer/settings.py
+++ theorizer/settings.py
@@ -16,7 +16,6 @@ import dj_database_url
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
