0cfe55bc72630444277bc0b73ae27b26ca541b80
Adrian Jewell <adrianjewell91@gmail.com>
2018-11-26 19:54:13 -0800

    update readme

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,27 @@
 path = require('path')
 
-  module.exports = {
-    context: __dirname,
-    entry: './frontend/entry.jsx',
-    output: {
-      path: path.resolve(__dirname, 'frontend','static','js'),
-      filename: 'bundle.js'
-    },
-    resolve: {
-      extensions: ['.js', '.jsx', '*']
-    },
-    module: {
-      loaders: [
-        {
-          test: /\.jsx?$/,
-          exclude: /(node_modules)/,
-          loader: 'babel-loader',
-          query: {
-            presets: ['es2015','react']
-          }
+module.exports = {
+  context: __dirname,
+  entry: './frontend/entry.jsx',
+  output: {
+    path: path.resolve(__dirname, 'frontend','static','js'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015','react']
         }
-      ]
-    },
-    devtool: 'source-maps',
-    watch: true
-  }
+      }
+    ]
+  },
+  devtool: 'source-maps',
+  watch: true
+}
